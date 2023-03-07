75dbc3f05d5b36deec4d8cbbcfed1bbc1076099e
Sonu Singh <chandrabhanusonu@gmail.com>
2023-03-07 23:38:40 +0530

    InitiliZion all file and image and some secion

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Lora': ['Lora', "serif"],
+        'Heebo': ['Heebo', "sans-serif"],
+        "Mullish": ['Mulish', 'sans-serif'],
+        "Ultra": ['Climate Crisis', "cursive"]
+      },
+    },
+  },
+  plugins: [],
+}
